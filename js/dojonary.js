1e481608943ba98831f3785295a6f3d07417c1d4
mauri-rivera <mauri.rivera@outlook.com>
2023-09-18 21:22:14 -0300

    Creación de index, dojonary y styles

---
+++ js/dojonary.js
@@ -0,0 +1,21 @@
+
+function agregarDefinicion(elemento) {
+    elemento.remove();
+}
+
+function agregarLike(elemento) {
+    let numero = parseInt(elemento.value.substr(0, 2));
+    numero++;
+
+    elemento.value = `${numero} likes`;
+}
+
+function presionarLogin(elemento) {
+    if (elemento.value === "Login") {
+        elemento.value = "Logout";
+    }
+    else {
+        elemento.value = "Login";
+    } 
+}
+
